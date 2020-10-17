4ce2ed041720d15ec4fe92dd8b4ad22e816e7fe1
Michal Piatkowski <piatkowski2002@gmail.com>
2020-10-18 01:52:19 +0200

    Fixed parsing of nested types

--- Sichem/ConversionResult.cs
+++ Sichem/ConversionResult.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Sichem
 {
 	public class ConversionResult
 	{
-		public readonly Dictionary<string, string> Enums = new Dictionary<string, string>();
-		public readonly Dictionary<string, string> Constants = new Dictionary<string, string>();
-		public readonly Dictionary<string, string> GlobalVariables = new Dictionary<string, string>();
-		public readonly Dictionary<string, string> Structs = new Dictionary<string, string>();
-		public readonly Dictionary<string, string> Methods = new Dictionary<string, string>();
+		public readonly Dictionary<string, StringBuilder> Enums = new Dictionary<string, StringBuilder>();
+
+		public readonly Dictionary<string, StringBuilder> Constants = new Dictionary<string, StringBuilder>();
+
+		public readonly Dictionary<string, StringBuilder> GlobalVariables = new Dictionary<string, StringBuilder>();
+
+		public readonly Dictionary<string, StringBuilder> Structs = new Dictionary<string, StringBuilder>();
+
+		public readonly Dictionary<string, StringBuilder> Methods = new Dictionary<string, StringBuilder>();
 	}
 }
